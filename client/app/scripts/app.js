aaeb5a7d01fff260b6bce8eba9d1014f503c8928
abrbhat <bhatnagarabhiroop@gmail.com>
2017-02-05 14:33:08 +0530

    Migrating to UI Router

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -13,39 +13,50 @@ var app = angular.module('crossoverCustomerSupportApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
-    'ngRoute',
+    'ui.router',
     'ngSanitize',
     'ngTouch',
     'ng-token-auth'
 ]);
 
-app.config(function ($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'views/main.html',
-      controller: 'MainCtrl',
-      controllerAs: 'main'
-    })
-    .when('/support_requests', {
-      templateUrl: 'components/support-request/list/template.html',
-      controller: 'SupportRequestListController',
-      resolve: {
-        auth: ['$auth', function($auth) {
-          return $auth.validateUser();
-        }]
-      }
-    })
-    .when('/sign_in', {
-        templateUrl: 'components/user/login/template.html',
-        controller: 'UserLoginController'
-      })
-    .when('/sign_up', {
-        templateUrl: 'components/user/registration/template.html',
-        controller: 'UserRegistrationController'
-      })
-    .otherwise({
-      redirectTo: '/'
-    });
+app.config(function ($stateProvider,$urlRouterProvider) {
+  $stateProvider
+
+  .state('home', {
+    url: '/home',
+    templateUrl: 'views/main.html',
+    controller: 'MainCtrl',
+    controllerAs: 'main'
+  });
+
+  // $routeProvider
+  //   .when('/', {
+  //     templateUrl: 'views/main.html',
+  //     controller: 'MainCtrl',
+  //     controllerAs: 'main'
+  //   })
+  //   .when('/support_requests', {
+  //     templateUrl: 'components/support-request/list/template.html',
+  //     controller: 'SupportRequestListController',
+  //     resolve: {
+  //       auth: ['$auth', function($auth) {
+  //         return $auth.validateUser();
+  //       }]
+  //     }
+  //   })
+  //   .when('/sign_in', {
+  //       templateUrl: 'components/user/login/template.html',
+  //       controller: 'UserLoginController'
+  //     })
+  //   .when('/sign_up', {
+  //       templateUrl: 'components/user/registration/template.html',
+  //       controller: 'UserRegistrationController'
+  //     })
+  //   .otherwise({
+  //     redirectTo: '/'
+  //   });
+
+  $urlRouterProvider.otherwise('/home');
 });
 
 app.run(['$rootScope', '$location', function($rootScope, $location) {
--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -9,6 +9,7 @@
  */
 angular.module('crossoverCustomerSupportApp')
   .controller('MainCtrl', function () {
+    console.log('here1');
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
