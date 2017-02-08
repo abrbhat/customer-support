62005b7a0fb749c1d39332b1fa7da3c2251cf9b5
abrbhat <bhatnagarabhiroop@gmail.com>
2017-02-08 14:06:17 +0530

    karma tests working

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -27,18 +27,19 @@ module.exports = function(config) {
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
 
       // other required libs
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-cookie/angular-cookie.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/ng-token-auth/dist/ng-token-auth.js',
 
       // app files
-      'app/scripts/**/*.js',
+      'app/app.js',
       'app/components/**/*.js',
+      'app/models/**/*.js',
       'app/**/*.html',
 
       'test/spec/controllers/**/*.js',
